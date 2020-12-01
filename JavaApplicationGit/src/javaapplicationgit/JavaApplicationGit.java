53334fdc8d54f77bc3e46770e019a5646b3efb29
Joan Grisó <Joan Gris@DESKTOP-17OS4J4>
2020-12-01 20:50:45 +0100

    Hello Git, commit por Joan el 01/12/2020 a las 20:50

--- JavaApplicationGit/src/javaapplicationgit/JavaApplicationGit.java
+++ JavaApplicationGit/src/javaapplicationgit/JavaApplicationGit.java
@@ -16,7 +16,7 @@ public class JavaApplicationGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello GIT");
+        System.out.println("Hello GitHub");
     }
     
 }
